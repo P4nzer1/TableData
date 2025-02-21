3ba42a6b53df532d116aee0aee482da58b5783b4
Nikita Smirov <nikitka-smirnov-2013@mail.ru>
2025-02-21 20:47:20 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://p4nzer1.github.io/TableData/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
